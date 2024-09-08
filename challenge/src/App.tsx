df2b25523226c57c82a2735eb7982be33e14f9a0
Ruan Lima <124522882+ruaanls@users.noreply.github.com>
2024-09-07 23:58:42 -0300

    Css base para a página inicial feito

--- challenge/src/App.tsx
+++ challenge/src/App.tsx
@@ -1,12 +1,13 @@
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
+
 import PaginaInicial from "./components/PaginaInicial/PaginaInicial";
 
 export default function App()
 {
   return(
     <div>
-      
+      <Header/>
       <PaginaInicial/>
       <Footer/>
     </div>
