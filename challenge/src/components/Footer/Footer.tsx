de8b4b6e6702142bcb52c1fec070f1ceeed5c71f
Ruan Lima <124522882+ruaanls@users.noreply.github.com>
2024-09-07 23:21:41 -0300

    CSS Footer Feito

--- challenge/src/components/Footer/Footer.tsx
+++ challenge/src/components/Footer/Footer.tsx
@@ -1,10 +1,11 @@
 import img from "../../../public/img/logo-porto.png"
+import styles from "../../footer.module.css"
 export default function Footer()
 {
     return(
-        <footer>
-            <img src={img} alt="" />
-            <h1>| Centro Automotivo <span>Porto</span></h1>
+        <footer className={styles.footer}>
+            <img src={img} alt="" className={styles.img} />
+            <h1> | Centro Automotivo <span>Porto</span></h1>
         </footer>
     )
 }
